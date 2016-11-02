692c4b786406a9f1a0af0085f9d909a9801326a6
OldFresco <hinsonkojo@gmail.com>
2016-11-02 01:53:54 +0000

    create template for cmdlets

---
+++ src/Sequensis.DevTools.Cmdlets/BrowserCmdlet.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Management.Automation;
+
+namespace Sequensis.DevTools.Cmdlets
+{
+    [Cmdlet("Start", "Browser")]
+    public class BrowserCmdlet : PSCmdlet
+    {
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ValueFromPipeline = true, Position = 0, HelpMessage = "Site to browse")]
+        [ValidateNotNullOrEmpty]
+        public string Site { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject("\n");
+            WriteObject("OPENING: " + Site);
+
+            Process.Start("http://google.com");
+        }
+    }
+}
